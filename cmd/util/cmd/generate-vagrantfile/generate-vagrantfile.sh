58e9516d9509e4a4ce0825afedb75147aca6235b
Mike Schinkel <mike@newclarity.net>
2017-05-19 19:37:31 -0400

    Finished 'box util generate-vagrantfile'

--- cmd/util/cmd/generate-vagrantfile/generate-vagrantfile.sh
+++ cmd/util/cmd/generate-vagrantfile/generate-vagrantfile.sh
@@ -4,41 +4,25 @@
 # See: https://serverfault.com/a/287690/101860
 #
 
-vars=(BOX_VERSION LOCAL_DOMAIN)
-exit
-
 project_dir="$(getProjectDir)"
+echo "${project_dir}"
 vagrant_file="${project_dir}/Vagrantfile"
 
 if [ -f "${vagrant_file}" ] ; then
-	if readNo "The file [${vagrant_file}] exists. Overwrite?" ; then 
+	if readNo "Overwrite existing ${vagrant_file}" ; then 
+		stdErr "User aborted."
 		exit 1
 	fi
 fi	
 
-BOX_VERSION="$(box util get-box-version)"
-LOCAL_DOMAIN="$(box util get-local-domain)"
-
-
-vars=(BOX_VERSION LOCAL_DOMAIN)
-box util load-template "Vagrantfile" "${vars[@]}"
-exit
-
-foo='BOX_VERSION'
-echo "${!foo}"
-exit
-
 tmp_file="${BOXCLI_TMP_DIR}/Vagrantfile"
 
-box util load-template "Vagrantfile" 
+box util load-template "Vagrantfile" > $tmp_file
 
+box util copy-to-project-dir "${tmp_file}" --quiet
 
-
-echo "${content}"
-
-#box util copy-to-project-dir "${tmp_file}"
-
-statusMsg "Vagrant file [${vagrant_file}] generated."
+statusMsg "Vagrant file generated: ${vagrant_file}"
+exit
 
 
 
